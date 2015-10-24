59bc507a4b300433d87d1ff2579b33bd7fa5db8d
dastuhl <maschdei@gmail.com>
2015-10-24 23:21:56 +0200

    Settings

---
+++ app/src/main/java/de/dastuhl/hours/SettingsFragment.java
@@ -0,0 +1,53 @@
+package de.dastuhl.hours;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    // required Constructor
+    public SettingsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+
+        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(),
+                getString(R.string.pref_max_value_days_key));
+        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(),
+                getString(R.string.pref_max_value_weeks_key));
+        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(),
+                getString(R.string.pref_max_value_months_key));
+        onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(),
+                getString(R.string.pref_max_value_years_key));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        Preference pref = findPreference(key);
+        if (pref instanceof EditTextPreference) {
+            pref.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
+}
